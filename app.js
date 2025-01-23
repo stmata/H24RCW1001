929812e0b801436a681a30ad2acfbc95c6132533
stmata <108939392+stmata@users.noreply.github.com>
2025-01-23 15:16:55 -0500

    Update app.js
    Added setTimeout function

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ for(i=0; i<=10; i++){
         console.log(i*j);
     }
 }
+
+setTimeout(() => {
+  console.log("this is the first message");
+}, 5000);
+setTimeout(() => {
+  console.log("this is the second message");
+}, 3000);
+setTimeout(() => {
+  console.log("this is the third message");
+}, 1000);
+
+// Output:
+
+// this is the third message
+// this is the second message
+// this is the first message
